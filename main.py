d930c3a95210864fac704df7c80ee12ee7c2669b
Nikolai Smirnow <smirnownb@gmail.com>
2019-12-23 08:31:55 -0700

    Revert "Revert "Added driver class, removed helpers""
    This reverts commit 0c05888873fd68c3f1886f0403ff005f015285db.

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
 """Execute this file to play tic-tac-toe."""
 
-from helpers import (
-    create_game,
-    pick_move_to_make,
-    execute_turn,
-)
+from driver import TicTacToeDriver
 
-GAME = create_game()
-while not GAME.winner:
-    MOVE = pick_move_to_make()
-    execute_turn(GAME, MOVE)
-    GAME.display_board()
-
-print('Congratulations! {}s wins!'.format(GAME.winner))
+DRIVER = TicTacToeDriver()
+DRIVER.run_game()
+print('Congratulations! {}s wins!'.format(DRIVER.game.winner))
