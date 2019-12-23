0c05888873fd68c3f1886f0403ff005f015285db
Nikolai Smirnow <smirnownb@gmail.com>
2019-12-23 08:22:34 -0700

    Revert "Added driver class, removed helpers"
    This reverts commit 9723064c2fae98afe7a5d41f52e20c289c6c5747.

---
+++ helpers.py
@@ -0,0 +1,53 @@
+"""Functions that should live somewhere else, but I couldn't find a place for them."""
+
+
+# TODO: Find a better way to implement this code than helper functions
+from tic_tac_toe import TicTacToe, Move
+import errors
+
+
+def create_game():
+    """Start a new game of tic-tac-toe.
+
+    :raise AssertionError: if player doesn't pick a non-zero integer for the size of the game
+    :return: A newly instantiated TicTacToe object
+    """
+
+    game = None
+    while not game:
+        try:
+            game_size = int(input('What size board would you like to play with?'))
+            game = TicTacToe(game_size)
+        except AssertionError:
+            print('Please pick an integer greater than 0')
+
+    return game
+
+
+def pick_move_to_make():
+    """Pick which row and column to make the next move in.
+
+    :returns Move: Move to be made
+    """
+    selecting_move = True
+    while selecting_move:
+        try:
+            row = int(input('In what row would you like to make your move?'))
+            column = int(input('In what column would you like to make your move?'))
+            return Move(row, column)
+        except ValueError:
+            print('You must pick integers for row and column.')
+
+
+def execute_turn(game, move):
+    """Player get to make a move, if it is legal.
+
+    :param TicTacToe game: The game of tic-tac-toe being played
+    :param Move move: The move the current player is attempting to make
+    """
+    try:
+        game.execute_turn(move)
+    except IndexError:
+        print('This move is out of bounds. Please pick a different row and/or column.')
+    except errors.CellInUseError:
+        print('This cell is occupied. Please pick a different row and/or column.')
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 """Execute this file to play tic-tac-toe."""
 
-from driver import TicTacToeDriver
+from helpers import (
+    create_game,
+    pick_move_to_make,
+    execute_turn,
+)
 
-DRIVER = TicTacToeDriver()
-DRIVER.run_game()
-print('Congratulations! {}s wins!'.format(DRIVER.game.winner))
+GAME = create_game()
+while not GAME.winner:
+    MOVE = pick_move_to_make()
+    execute_turn(GAME, MOVE)
+    GAME.display_board()
+
+print('Congratulations! {}s wins!'.format(GAME.winner))
