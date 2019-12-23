9723064c2fae98afe7a5d41f52e20c289c6c5747
Nikolai Smirnow <smirnownb@gmail.com>
2019-12-23 07:42:46 -0700

    Added driver class, removed helpers

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,5 +1,5 @@
 """It's tic-tac-toe."""
-import errors
+# TODO: make getters for row and diagonals, have them return lists, for use in driver.py
 
 
 class TicTacToe:
@@ -34,79 +34,31 @@ class TicTacToe:
 
         :return: Empty board
         """
+        # TODO: google factory pattern
         board = [None] * self.game_size
         for row in range(self.game_size):
             board[row] = [" " for _ in range(self.game_size)]
         print(board)
         return board
 
-    def execute_turn(self, move):
-        """Make move, check if game has been won, and if not change the current player
+    def get_contents_of_cell(self, coordinates):
+        """Get the contents of the cell with the specified coordinates"""
+        return self.board[coordinates.row][coordinates.column]
 
-        :param Move move: Move with which to update the board
-        """
-        self.__make_move(move)
-        self.__check_if_game_is_over()
-        if not self.winner:
-            self.__change_player()
-
-    def __make_move(self, move):
+    def make_move(self, coordinates):
         """Update the game board with a move.
 
-        :param Move move: Move with which to update the board
+        :param Coordinates coordinates: Coordinates at which to place move
         """
-        if self.board[move.row][move.column] != ' ':
-            raise errors.CellInUseError('This cell has already been used.')
-        self.board[move.row][move.column] = self.current_player
+        self.board[coordinates.row][coordinates.column] = self.current_player
 
-    def __check_if_game_is_over(self):
-        self.__check_rows_for_win()
-        self.__check_columns_for_win()
-        self.__check_upper_left_to_lower_right_diagonal_for_win()
-        self.__check_lower_left_to_upper_right_diagonal_for_win()
-        if not self.board_has_free_cells:
-            self.winner = 'No one'
-
-    def __check_rows_for_win(self):
-        for row in self.board:
-            if self.__all_moves_are_equal(moves=row):
-                self.winner = self.current_player
-
-    def __check_columns_for_win(self):
-        for column_index in range(self.game_size):
-            column = self.__get_column(column_index)
-            if self.__all_moves_are_equal(moves=column):
-                self.winner = self.current_player
-
-    def __get_column(self, column_index):
+    def get_column(self, column_index):
         column = []
         for row in self.board:
             column.append(row[column_index])
         return column
 
-    def __check_upper_left_to_lower_right_diagonal_for_win(self):
-        diagonal = []
-        for _ in range(self.game_size):
-            next_move = self.board[_][_]
-            diagonal.append(next_move)
-        if self.__all_moves_are_equal(moves=diagonal):
-            self.winner = self.current_player
-
-    def __check_lower_left_to_upper_right_diagonal_for_win(self):
-        diagonal = []
-        for _ in range(self.game_size):
-            next_move = self.board[self.game_size - _ - 1][_]
-            diagonal.append(next_move)
-        if self.__all_moves_are_equal(moves=diagonal):
-            self.winner = self.current_player
-
-    def __all_moves_are_equal(self, moves):
-        first_move = moves[0]
-        if first_move != ' ':
-            all_moves_are_the_same = all([moves[0] == move for move in moves])
-            return all_moves_are_the_same
-
-    def __change_player(self):
+    def change_player(self):
         if self.current_player == 'X':
             self.current_player = 'O'
         else:
@@ -119,14 +71,14 @@ class TicTacToe:
 
 
 # pylint: disable=too-few-public-methods
-class Move:
-    """A move in tic-tac-toe."""
+class Coordinates:
+    """A pair of coordinates on a tic-tac-toe board."""
 
     def __init__(self, row, column):
-        """Initialize move.
+        """Initialize coordinates.
 
-        :param int row: Row in which to place move
-        :param int column: Column in which to place move
+        :param int row: Row on board
+        :param int column: Column on board
         """
         self.row = row
         self.column = column
